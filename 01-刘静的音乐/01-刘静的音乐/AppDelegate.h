4a1173b9c10a6839b7bb8aa27a36be2a6f8454ae
Your Name <469836293@qq.com>
2015-11-02 17:13:51 +0800

    初始化项目

---
+++ 01-刘静的音乐/01-刘静的音乐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-刘静的音乐
+//
+//  Created by 刘静 on 15/11/2.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-刘静的音乐/01-刘静的音乐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-刘静的音乐
+//
+//  Created by 刘静 on 15/11/2.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

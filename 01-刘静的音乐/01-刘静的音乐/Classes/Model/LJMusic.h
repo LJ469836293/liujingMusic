74efd9f2f3389aebe73941dee2715969673db512
Your Name <469836293@qq.com>
2015-11-02 18:20:34 +0800

    框架资源

---
+++ 01-刘静的音乐/01-刘静的音乐/Classes/Model/LJMusic.h
@@ -0,0 +1,13 @@
+//
+//  LJMusic.h
+//  01-刘静的音乐
+//
+//  Created by 刘静 on 15/11/2.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJMusic : NSObject
+
+@end
---
+++ 01-刘静的音乐/01-刘静的音乐/Classes/Model/LJMusic.m
@@ -0,0 +1,13 @@
+//
+//  LJMusic.m
+//  01-刘静的音乐
+//
+//  Created by 刘静 on 15/11/2.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import "LJMusic.h"
+
+@implementation LJMusic
+
+@end

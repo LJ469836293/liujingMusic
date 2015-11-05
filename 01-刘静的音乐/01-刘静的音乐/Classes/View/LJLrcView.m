a5787b3e5aa2f7ae87152a3ee023bc8ba2447e0e
liujing <123425364u@qq.com>
2015-11-05 22:44:11 +0800

    添加分类

---
+++ 01-刘静的音乐/01-刘静的音乐/Classes/View/LJLrcView.m
@@ -0,0 +1,21 @@
+//
+//  LJLrcView.m
+//  01-刘静的音乐
+//
+//  Created by 刘静 on 15/11/5.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import "LJLrcView.h"
+
+@implementation LJLrcView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
